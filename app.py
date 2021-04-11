de591c36434c4a588d043572724c44ff62feb184
FedePreto <s1092769@users.noreply.github.com>
2021-04-11 21:38:19 +0200

    Implementato altro codice nella classe app.py
    Ora se i dati di Login sono corretti si apre la pagina principale

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtWidgets import QApplication, QDialog, QWidget, QPushButton
 from interfacce import *
 from interfacce.login_window_ui import Ui_Dialog_Login
+from interfacce.main_window import Ui_MainWindow
 
 
 #prova
@@ -21,10 +22,14 @@ class Window_Login(QtWidgets.QMainWindow, Ui_Dialog_Login):
         pw=self.lineEdit_User_2.text()
         if((user=="admin")&(pw=="admin")):
             print ("Accesso eseguito")
-            
+            self.main_window = QtWidgets.QMainWindow()
+            self.ui = Ui_MainWindow()
+            self.ui.setupUi(self.main_window)
+            self.main_window.show()
+            self.hide()
         else: 
             print("Accesso fallito")
-            window_error= Ui_Login_Error(self)
+            window_error = Ui_Login_Error(self)
             window_error.exec()
 
 
@@ -32,6 +37,7 @@ class Ui_Login_Error(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         uic.loadUi('ui\login_error.ui', self)
+
 
 if __name__ == "__main__":
     app = QApplication([])
