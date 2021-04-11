c564311d73c072616dc3bd791523a501c077be5d
FedePreto <s1092769@users.noreply.github.com>
2021-04-11 20:41:21 +0200

    Programma complessivo

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-import sys
-
-import PyQt5 
-from PyQt5 import QtCore, QtGui, QtWidgets
+import sys 
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtWidgets import QApplication, QDialog, QWidget, QPushButton
 from interfacce import *
 from interfacce.login_window_ui import Ui_Dialog_Login
-from PyQt5 import uic
+
 
 #prova
     
 username="admin"
 password="admin"
 
-class Window_Login(QtWidgets.QMainWindow, Ui_Dialog_Login, QDialog):
+class Window_Login(QtWidgets.QMainWindow, Ui_Dialog_Login):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
@@ -26,8 +24,14 @@ class Window_Login(QtWidgets.QMainWindow, Ui_Dialog_Login, QDialog):
             
         else: 
             print("Accesso fallito")
-            uic.loadUi("ui/login_error.ui")
-      
+            window_error= Ui_Login_Error(self)
+            window_error.exec()
+
+
+class Ui_Login_Error(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        uic.loadUi('ui\login_error.ui', self)
 
 if __name__ == "__main__":
     app = QApplication([])
