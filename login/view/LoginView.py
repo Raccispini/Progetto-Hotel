bb3dbbe6c56901928a6d61a720e79547a2fec720
FedePreto <s1092769@users.noreply.github.com>
2021-05-08 20:46:43 +0200

    Collegamenti con le interfacce

--- login/view/LoginView.py
+++ login/view/LoginView.py
@@ -1,6 +1,5 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets
 
-from home.view.HomeView import HomeView
 from interfacce.main_window_ui import Window_Main
 from login.model.LoginModel import LoginModel
 from login.view.login_window_ui import Ui_Dialog_Login
@@ -16,15 +15,9 @@ class LoginView(QtWidgets.QMainWindow, Ui_Dialog_Login):
     def accesso(self):
         login_model = LoginModel(self.lineEdit_User.text(),self.lineEdit_User_2.text())
         if login_model.accedi:
-            self.home = HomeView()
-            self.home.show()
-            self.hide()
-
-            '''
             main_window = Window_Main()
             main_window.show()
             self.hide()
-            '''
         else:
             # da mettere la finestra di errore
             print("errore")
