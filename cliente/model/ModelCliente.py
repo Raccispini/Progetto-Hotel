ae3312a4ae356145851ba54d1fb4ba9680e96eb1
FedePreto <s1092769@users.noreply.github.com>
2021-05-24 22:33:09 +0200

    Aggiunti Utenti e Clienti e modificata classe Meteo

---
+++ cliente/model/ModelCliente.py
@@ -0,0 +1,116 @@
+
+class ModelCliente(ModelUtente):
+    def __init__(self, id, nome, cognome, sesso, data_di_nascita, luogo_di_nascita, residenza, provincia, via, cap, cf, nazione, telefono, cellulare, email, tipo_documento, numero_documento, ente_rilascio, data_rilascio, modalita_pagamento, info_check_in):
+        super(ModelCliente, self).__init__(nome, cognome, email, cellulare, data_di_nascita)
+        self.id = id
+        self.sesso = sesso
+        self.luogo_di_nascita = luogo_di_nascita
+        self.residenza = residenza
+        self.provincia = provincia
+        self.via = via
+        self.cap = cap
+        self.cf = cf
+        self.nazione = nazione
+        self.telefono = telefono
+        self.tipo_documento = tipo_documento
+        self.numero_documento = numero_documento
+        self.ente_rilascio = ente_rilascio
+        self.data_rilascio = data_rilascio
+        self.modalita_pagamento = modalita_pagamento
+        self.info_check_in = info_check_in
+
+    def get_id(self):
+        return self.id
+
+    def get_sesso(self):
+        return self.sesso
+
+    def get_luogo_di_nascita(self):
+        return self.luogo_di_nascita
+
+    def get_residenza(self):
+        return self.residenza
+
+    def get_provincia(self):
+        return self.provincia
+
+    def get_via(self):
+        return self.via
+
+    def get_cap(self):
+        return self.cap
+
+    def get_cf(self):
+        return self.cf
+
+    def get_nazione(self):
+        return self.nazione
+
+    def get_telefono(self):
+        return self.telefono
+
+    def get_tipo_documento(self):
+        return self.tipo_documento
+
+    def get_numero_documento(self):
+        return self.numero_documento
+
+    def get_ente_rilascio(self):
+        return self.ente_rilascio
+
+    def get_data_rilascio(self):
+        return self.data_rilascio
+
+    def get_modalita_pagamento(self):
+        return self.modalita_pagamento
+
+    def get_info_check_in(self):
+        return self.info_check_in
+
+    def set_id(self, id):
+        self.id = id
+
+    def set_sesso(self, sesso):
+        self.sesso = sesso
+
+    def set_luogo_di_nascita(self, luogo_di_nascita):
+        self.luogo_di_nascita = luogo_di_nascita
+
+    def set_residenza(self, residenza):
+        self.residenza = residenza
+
+    def set_provincia(self, provincia):
+        self.provincia = provincia
+
+    def set_via(self, via):
+        self.via = via
+
+    def set_cap(self, cap):
+        self.cap = cap
+
+    def set_cf(self, cf):
+        self.cf = cf
+
+    def set_nazione(self, nazione):
+        self.nazione = nazione
+
+    def set_telefono(self, telefono):
+        self.telefono = telefono
+
+    def set_tipo_documento(self, tipo_documento):
+        self.tipo_documento = tipo_documento
+
+    def set_numero_documento(self, numero_documento):
+        self.numero_documento = numero_documento
+
+    def set_ente_rilascio(self, ente_rilascio):
+        self.ente_rilascio = ente_rilascio
+
+    def set_data_rilascio(self, data_rilascio):
+        self.data_rilascio = data_rilascio
+
+    def set_modalita_pagamento(self, modalita_pagamento):
+        self.modalita_pagamento = modalita_pagamento
+
+    def set_info_check_in(self, info_check_in):
+        self.info_check_in = info_check_in
--- meteo/view/MeteoView.py
+++ meteo/view/MeteoView.py
@@ -16,8 +16,8 @@ class MeteoView(QDialog):
         font_label.setBold(True)
 
         lB_message.setFont(font_label)
-        le_citta = QLineEdit()
-        font_lineEdit = le_citta.font()
+        self.le_citta = QLineEdit()
+        font_lineEdit = self.le_citta.font()
         font_lineEdit.setPointSize(15)
         pulsante_Continua=QPushButton()
         pulsante_Annulla=QPushButton()
@@ -25,7 +25,7 @@ class MeteoView(QDialog):
         pulsante_Continua.setText("Continua")
         pulsante_Annulla.setText("Annulla")
 
-        font_button_C=pulsante_Continua.font()
+        font_button_C= pulsante_Continua.font()
         font_button_A = pulsante_Annulla.font()
         font_button_A.setPointSize(11)
         font_button_C.setPointSize(11)
@@ -37,11 +37,11 @@ class MeteoView(QDialog):
         pulsante_Continua.setMinimumSize(250,30)
         pulsante_Annulla.setMaximumSize(271, 30)
         pulsante_Annulla.setMinimumSize(271, 30)
-        le_citta.setFont(font_lineEdit)
+        self.le_citta.setFont(font_lineEdit)
         h_layout.addWidget(pulsante_Continua)
         h_layout.addWidget(pulsante_Annulla)
         v_layout.addWidget(lB_message)
-        v_layout.addWidget(le_citta)
+        v_layout.addWidget(self.le_citta)
         v_layout.addLayout(h_layout)
 
         self.setLayout(v_layout)
@@ -51,12 +51,10 @@ class MeteoView(QDialog):
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("ui\\resources/logo/logo_small_icon_only_inverted.png"), QtGui.QIcon.Normal)
         self.setWindowIcon(icon)
+
     def ricerca(self):
+        self.le_citta.te
 
 
-if __name__ == '__main__':
-    app=QApplication(sys.argv)
-    meteo=MeteoView()
-    meteo.show()
-    sys.exit(app.exec())
+
 
