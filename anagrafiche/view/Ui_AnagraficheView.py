434337357e78ac132fcdb7620545f82333d77d84
Nicolò <s1092919@studenti.univpm.it>
2021-06-30 16:56:17 +0200

    correzione errore anagrafiche

--- anagrafiche/view/Ui_AnagraficheView.py
+++ anagrafiche/view/Ui_AnagraficheView.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Anagrafiche_Window(object):
+class Ui_AnagraficheView(object):
     def setupUi(self, Anagrafiche_Window):
         Anagrafiche_Window.setObjectName("Anagrafiche_Window")
         Anagrafiche_Window.resize(1315, 821)
