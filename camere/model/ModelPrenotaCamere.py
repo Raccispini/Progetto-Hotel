9599370d8f94a23ba7d6a190a24d2358ba126335
Nicolò <s1092919@studenti.univpm.it>
2021-07-11 17:08:25 +0200

    Camere funzionanti con prenotazioni
    manca il preventivo e il tasto annulla sulla prenotazione

--- camere/model/ModelPrenotaCamere.py
+++ camere/model/ModelPrenotaCamere.py
@@ -1,6 +1,20 @@
+import sqlite3
 class ModelPrenotaCamere:
 	def __init__(self):
 		pass
 	@staticmethod
 	def getClienti():
+		con = sqlite3.connect("database.db")
+		query = "SELECT * FROM Clienti Order By Clienti.ID"
+		#con.close()
+		return con.execute(query).fetchall()
+	@staticmethod
+	def prenota(check_in,check_out,data,camera):
+		con = sqlite3.connect("database.db")
+		query = "INSERT INTO Prenotazioni_camere(id_camere,check_in,check_out,data_prenotazione) VALUES ("+str(camera)+",'"+str(check_in)+"','"+str(check_out)+"','"+str(data)+"');"
+		con.execute(query)
+		con.commit()
+
+
+
 		
